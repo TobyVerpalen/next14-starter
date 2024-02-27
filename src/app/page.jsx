883bf9126082feb2bd16952e58710ce1efdcd978
TobyVerpalen <tobyverpalen@gmail.com>
2024-02-27 13:17:55 +0100

    new pages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 const Home = () => {
-  return <div>Hello World!</div>;
+  return <div>Homepage</div>;
 };
 
 export default Home;
+
+ git config --global user.name "TobyVerpalen"
+ git config --global user.email tobyverpalen@gmail.com
